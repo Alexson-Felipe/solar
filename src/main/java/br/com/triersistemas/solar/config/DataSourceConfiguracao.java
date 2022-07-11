7ad20637cfb26a8bf313f31fc5e1c697c300f828
Alexson Felipe <alexfelipesilveira@gmail.com>
2022-07-11 15:28:22 -0300

    Ajuste

--- src/main/java/br/com/triersistemas/solar/config/DataSourceConfiguracao.java
+++ src/main/java/br/com/triersistemas/solar/config/DataSourceConfiguracao.java
@@ -11,7 +11,12 @@ public class DataSourceConfiguracao {
 
     @Bean
     public DataSource dataSource(){
-        String url = "jdbc:postgresql://localhost:5434/solar"; //porta da esquerda no container docker 5434:5432
+        //Trier
+        //String url = "jdbc:postgresql://localhost:5434/solar"; //porta da esquerda no container docker 5434:5432
+
+        //Em casa
+        String url = "jdbc:postgresql://localhost:5432/solar"; //porta da esquerda no container docker 5434:5432
+
         String usuario = "postgres";
         String senha = "postgres";
 
