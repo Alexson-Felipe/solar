12a74058fed720718f9c615816153a96a65a0ccf
Alexson-Felipe <alexfelipesilveira@gmail.com>
2022-07-11 11:47:53 -0300

    Ajuste

---
+++ src/main/java/br/com/triersistemas/solar/config/DataSourceConfiguracao.java
@@ -0,0 +1,20 @@
+package br.com.triersistemas.solar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguracao {
+
+    @Bean
+    public DataSource dataSource(){
+        String url = "jdbc:postgresql://localhost:5434/solar"; //porta da esquerda no container docker 5434:5432
+        String usuario = "postgres";
+        String senha = "postgres";
+
+        return new DriverManagerDataSource(url, usuario, senha);
+    }
+}
